63defeccc757f18ade72dc93003b69f3322c75dd
Max Gamel <gamelmagdy2.0@gmail.com>
2023-01-15 21:57:09 -0500

    sign up

---
+++ iam-3-sign-up-and-log-in/README.md
@@ -0,0 +1,1 @@
+Project REST-rant
---
+++ iam-3-sign-up-and-log-in/backend/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express();
+
+
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use('/authentication', require('./controllers/authentication'))
+
+
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/places', require('./controllers/places'))
+app.use('/users', require('./controllers/users'))
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on ${process.env.PORT}`)
+})]
